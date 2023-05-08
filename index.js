731dbb85de9705e09dad5e938acac4257a50b4bd
Quentin Horgues <77660464+qhorgues@users.noreply.github.com>
2023-05-08 14:21:10 +0200

    Ajout annonce BO

--- index.js
+++ index.js
@@ -46,6 +46,16 @@ client.on(Events.InteractionCreate, async interaction => {
 	}
 });
 
+client.on('threadCreate', post => {
+    
+    const ForumBO_Id = "1105065775703785623";
+    if (post.parentId === ForumBO_Id && post.type === ChannelType.PublicThread)
+    {
+        const AnnounceChannel = client.channels.cache.get('1084514373038522428');
+        AnnounceChannel.send("<@" + post.ownerId + "> vient de publier le build order : \n" + post.name + " dans <#" + post.parentId + ">");
+    }
+})
+
 client.on("ready", () => {
     console.log("bot operatrionnel");  
 })
