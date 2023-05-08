b1ef6b1553936974fb7e8af011fb8f454a586339
Quentin Horgues <77660464+qhorgues@users.noreply.github.com>
2023-05-08 13:54:32 +0200

    ajout commandes user

---
+++ commands/utility/user.js
@@ -0,0 +1,12 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('user')
+		.setDescription('Provides information about the user.'),
+	async execute(interaction) {
+		// interaction.user is the object representing the User who ran the command
+		// interaction.member is the GuildMember object, which represents the user in the specific guild
+		await interaction.reply(`Cette commande a été exécutée par ${interaction.user.username}, qui a rejoint le ${interaction.member.joinedAt}.`);
+	},
+};
