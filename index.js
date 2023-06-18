f0514209113afb4d12de411118f6c038c4c174e0
Quentin Horgues <77660464+qhorgues@users.noreply.github.com>
2023-06-18 20:04:52 +0200

    Prise en charge du notif Bo

--- index.js
+++ index.js
@@ -53,8 +53,9 @@ client.on('threadCreate', post => {
 	const AnnounceChannel_Id = "1080504576328880151";
     if (post.parentId === ForumBO_Id && post.type === ChannelType.PublicThread)
     {
+		const NotifBO_Id = "1110631444432306288";
         const AnnounceChannel = client.channels.cache.get(AnnounceChannel_Id);
-        AnnounceChannel.send("||@everyone||\nOyé, oyé, bande de troubadours assoiffés de BO. <@" + post.ownerId + "> vient de nous sortir un nouveau breuvage baptisé ***" + post.name + 
+        AnnounceChannel.send("||<@" + NotifBO_Id + ">||\nOyé, oyé, bande de troubadours assoiffés de BO. <@" + post.ownerId + "> vient de nous sortir un nouveau breuvage baptisé ***" + post.name + 
                                 "***, servez-vous dans vos auges et abreuvez-vous de ce savoir : <#" + post.id + ">")
     }
 	else if (post.parentId === ForumCommu_Id && post.type === ChannelType.PublicThread)
