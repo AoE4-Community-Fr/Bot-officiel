f731fcb12c2b4f52cbd8618cb16ca4c770e53656
Quentin Horgues <77660464+qhorgues@users.noreply.github.com>
2023-06-23 16:58:54 +0200

    ajout de test pour la redaction d'un embed message

--- index.js
+++ index.js
@@ -22,85 +22,85 @@ for (const folder of commandFolders) {
 }
 
 client.on(Events.InteractionCreate, async interaction => {
-
+	
 	if (interaction.type === InteractionType.ApplicationCommand) {
 		const command = interaction.client.commands.get(interaction.commandName);
-
+		
 		if (interaction.commandName === "message") {
-
-
+			
+			
 			const modal = new ModalBuilder()
-				.setCustomId('Modal_Message')
-				.setTitle('Nouveau message');
+			.setCustomId('Modal_Message')
+			.setTitle('Nouveau message');
 
 			/* const channel = new ChannelSelectMenuBuilder({
 				custom_id: 'dst_channel',
 			})
 			.setChannelTypes(ChannelType.GuildText, ChannelType.GuildAnnouncement)
- */
+			*/
 			const message = new TextInputBuilder()
-				.setCustomId('classicMessage')
-				.setLabel("Rediger un message classic")
-				.setRequired(false)
-				.setStyle(TextInputStyle.Paragraph);
-
+			.setCustomId('classicMessage')
+			.setLabel("Rediger un message classic")
+			.setRequired(false)
+			.setStyle(TextInputStyle.Paragraph);
+			
 			const firstActionRow = new ActionRowBuilder().addComponents(message);
-
+			
 			// Add inputs to the modal
 			modal.addComponents(firstActionRow);
-
+			
 			// Show the modal to the user
 			await interaction.showModal(modal);
 		} else if (interaction.commandName === "embed_message") {
 			const modalEmbed = new ModalBuilder()
-				.setCustomId('Modal_embedMessage')
-				.setTitle('Nouveau message formatté');
-
+			.setCustomId('Modal_embedMessage')
+			.setTitle('Nouveau message formatté');
+			
 			const embededMessageAuthor = new TextInputBuilder()
-				.setCustomId('embededMessageAuthor')
-				.setLabel("Quel est l'auteur du message formatté")
-				.setRequired(false)
-				.setStyle(TextInputStyle.Short);
-
+			.setCustomId('embededMessageAuthor')
+			.setLabel("Quel est l'auteur du message formatté")
+			.setRequired(false)
+			.setStyle(TextInputStyle.Short);
+			
 			const embededMessageAuthorIcone = new TextInputBuilder()
-				.setCustomId('embededMessageAuthorIcon')
-				.setLabel("Quel est l'icone de l'auteur")
-				.setRequired(false)
-				.setStyle(TextInputStyle.Short);
-
+			.setCustomId('embededMessageAuthorIcon')
+			.setLabel("Quel est l'icone de l'auteur")
+			.setRequired(false)
+			.setStyle(TextInputStyle.Short);
+			
 			const embededMessageTitle = new TextInputBuilder()
-				.setCustomId('embededMessageTitle')
-				.setLabel("Quel est le titre du message formaté")
-				.setRequired(false)
-				.setStyle(TextInputStyle.Short);
-
+			.setCustomId('embededMessageTitle')
+			.setLabel("Quel est le titre du message formaté")
+			.setRequired(false)
+			.setStyle(TextInputStyle.Short);
+			
 			const embededMessage = new TextInputBuilder()
-				.setCustomId('embededMessage')
-				.setLabel("Rediger à la suite un message formaté")
-				.setRequired(true)
-				.setStyle(TextInputStyle.Paragraph);
-
+			.setCustomId('embededMessage')
+			.setLabel("Rediger à la suite un message formaté")
+			.setRequired(true)
+			.setStyle(TextInputStyle.Paragraph);
+			
 			const embededMessageImage = new TextInputBuilder()
-				.setCustomId('image')
-				.setLabel("Image principale")
-				.setRequired(false)
-				.setStyle(TextInputStyle.Short);
-
+			.setCustomId('image')
+			.setLabel("Image principale")
+			.setRequired(false)
+			.setStyle(TextInputStyle.Short);
+			
 			const firstActionRow = new ActionRowBuilder().addComponents(embededMessageAuthor);
 			const secondActionRow = new ActionRowBuilder().addComponents(embededMessageAuthorIcone);
 			const thirdActionRow = new ActionRowBuilder().addComponents(embededMessageTitle);
 			const fourthActionRow = new ActionRowBuilder().addComponents(embededMessage);
 			const firthActionRow = new ActionRowBuilder().addComponents(embededMessageImage);
-
+			
 			modalEmbed.addComponents(firstActionRow, secondActionRow, thirdActionRow, fourthActionRow, firthActionRow);
-
+			
 			await interaction.showModal(modalEmbed);
-
+			
 		} else if (!command) {
 			console.error(`No command matching ${interaction.commandName} was found.`);
 			return;
 		} else {
-
+			
 			try {
 				await command.execute(interaction);
 			} catch (error) {
@@ -115,61 +115,64 @@ client.on(Events.InteractionCreate, async interaction => {
 	} else if (interaction.type === InteractionType.ModalSubmit) {
 		if (interaction.customId === 'Modal_Message') {
 			const classicMesage = interaction.fields.getTextInputValue('classicMessage');
-
+			
 			interaction.channel.send(classicMesage);
 			interaction.reply({ content: 'Message envoyé', ephemeral: true });
 		}
 		else if (interaction.customId === 'Modal_embedMessage') {
 			const author = interaction.fields.getTextInputValue('embededMessageAuthor');
 			const authorIcon = interaction.fields.getTextInputValue('embededMessageAuthorIcon');
-			let Author = 'AOE 4 - Communauté FR';
-			let icon = 'https://raw.githubusercontent.com/AoE4-Community-Fr/Assets/master/Logo_serveur.png'
-
-			if (!(author === '' || author === 'AOE 4 - Communauté FR')) {
-				Author = author;
-				icon = authorIcon;
-			}
-
-			const title = interaction.fields.getTextInputValue('embededMessageTitle');
-			const message = interaction.fields.getTextInputValue('embededMessage');
-			const image = interaction.fields.getTextInputValue('image');
-
-
-			const embed = new EmbedBuilder()
+			let Author = author;
+			let icon = authorIcon;
+			// let Author = 'AOE 4 - Communauté FR';
+			// let icon = 'https://raw.githubusercontent.com/AoE4-Community-Fr/Assets/master/Logo_serveur.png'
+			
+			// if (!(author === '' || author === 'AOE 4 - Communauté FR')) {
+				// 	Author = author;
+				// 	icon = authorIcon;
+				// }
+				
+				const title = interaction.fields.getTextInputValue('embededMessageTitle');
+				const message = interaction.fields.getTextInputValue('embededMessage');
+				const image = interaction.fields.getTextInputValue('image');
+				
+				
+				const embed = new EmbedBuilder()
 				.setColor(0x7a7ae0)
-				.setTitle(title)
-				.setAuthor({ name: Author, iconURL: icon })
 				.setDescription(message)
-
-			if (image !== '')
-			{
+				
+				if (Author !== '') {
+					embed.setAuthor({ name: Author, iconURL: icon })
+			}
+			if (image !== '') {
 				embed.setImage(image)
+			}
+			if (title !== '') {
+				embed.setTitle(title)
 			}
 			
 			interaction.channel.send({ embeds: [embed] });
 			interaction.reply({ content: 'Message envoyé', ephemeral: true });
 		}
 	}
-
+	
 });
 
 client.on('threadCreate', post => {
-
+	
 	const ForumBO_Id = "1080894013550366730";
 	const ForumCommu_Id = "1081740810195832852";
 	const AnnounceChannel_Id = "1080504576328880151";
-    if (post.parentId === ForumBO_Id && post.type === ChannelType.PublicThread)
-    {
+	if (post.parentId === ForumBO_Id && post.type === ChannelType.PublicThread) {
 		const NotifBO_Id = "1110631444432306288";
-        const AnnounceChannel = client.channels.cache.get(AnnounceChannel_Id);
-        AnnounceChannel.send("||<@" + NotifBO_Id + ">||\nOyé, oyé, bande de troubadours assoiffés de BO. <@" + post.ownerId + "> vient de nous sortir un nouveau breuvage baptisé ***" + post.name + 
-                                "***, servez-vous dans vos auges et abreuvez-vous de ce savoir : <#" + post.id + ">")
-    }
-	else if (post.parentId === ForumCommu_Id && post.type === ChannelType.PublicThread)
-	{
 		const AnnounceChannel = client.channels.cache.get(AnnounceChannel_Id);
 		AnnounceChannel.send("||<@" + NotifBO_Id + ">||\nOyé, oyé, bande de troubadours assoiffés de BO. <@" + post.ownerId + "> vient de nous sortir un nouveau breuvage baptisé ***" + post.name +
-			"***, servez-vous dans vos auges et abreuvez-vous de ce savoir : <#" + post.id + ">")
+		"***, servez-vous dans vos auges et abreuvez-vous de ce savoir : <#" + post.id + ">")
+	}
+	else if (post.parentId === ForumCommu_Id && post.type === ChannelType.PublicThread) {
+		const AnnounceChannel = client.channels.cache.get(AnnounceChannel_Id);
+		AnnounceChannel.send("||<@" + NotifBO_Id + ">||\nOyé, oyé, bande de troubadours assoiffés de BO. <@" + post.ownerId + "> vient de nous sortir un nouveau breuvage baptisé ***" + post.name +
+		"***, servez-vous dans vos auges et abreuvez-vous de ce savoir : <#" + post.id + ">")
 	}
 	else if (post.parentId === ForumCommu_Id && post.type === ChannelType.PublicThread) {
 		const AnnounceChannel = client.channels.cache.get(AnnounceChannel_Id);
