4e52e3b1761e3e728b74a81bf5d671a1e8d7568f
Quentin Horgues <77660464+qhorgues@users.noreply.github.com>
2023-07-08 12:39:07 +0200

    Correction de bug du ping

--- index.js
+++ index.js
@@ -164,7 +164,7 @@ client.on('threadCreate', post => {
 	const ForumCommu_Id = "1081740810195832852";
 	const AnnounceChannel_Id = "1080504576328880151";
 	if (post.parentId === ForumBO_Id && post.type === ChannelType.PublicThread) {
-		const NotifBO_Id = "1110631444432306288";
+		const NotifBO_Id = "&1110631444432306288";
 		const AnnounceChannel = client.channels.cache.get(AnnounceChannel_Id);
 		AnnounceChannel.send("||<@" + NotifBO_Id + ">||\nOyé, oyé, bande de troubadours assoiffés de BO. <@" + post.ownerId + "> vient de nous sortir un nouveau breuvage baptisé ***" + post.name +
 		"***, servez-vous dans vos auges et abreuvez-vous de ce savoir : <#" + post.id + ">")
