b592aa6326d4a9587a5b5eca5c202d93677dec92
Quentin Horgues <quentin.horgues@outlook.fr>
2024-05-13 18:40:15 +0200

    changement sur le bot

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { Client, Collection, Events, GatewayIntentBits, ChannelType, InteractionT
 const { ModalBuilder, TextInputBuilder, TextInputStyle, ActionRowBuilder, EmbedBuilder } = require('discord.js');
 const { token } = require('./config.json');
 const { ForumBO_Id, ForumCommu_Id, AnnounceChannel_Id } = require('./discord-AoE4.json')
-const {replaceNitroEmoji} = require('./modules/replaceEmoji.js');
+const { replaceNitroEmoji } = require('./modules/replaceEmoji.js');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds], disableEveryone: false });
 
@@ -24,16 +24,16 @@ for (const folder of commandFolders) {
 }
 
 client.on(Events.InteractionCreate, async interaction => {
-	
+
 	if (interaction.type === InteractionType.ApplicationCommand) {
 		const command = interaction.client.commands.get(interaction.commandName);
-		
+
 		if (interaction.commandName === "message") {
-			
-			
+
+
 			const modal = new ModalBuilder()
-			.setCustomId('Modal_Message')
-			.setTitle('Nouveau message');
+				.setCustomId('Modal_Message')
+				.setTitle('Nouveau message');
 
 			/* const channel = new ChannelSelectMenuBuilder({
 				custom_id: 'dst_channel',
@@ -41,68 +41,68 @@ client.on(Events.InteractionCreate, async interaction => {
 			.setChannelTypes(ChannelType.GuildText, ChannelType.GuildAnnouncement)
 			*/
 			const message = new TextInputBuilder()
-			.setCustomId('classicMessage')
-			.setLabel("Rediger un message classic")
-			.setRequired(false)
-			.setStyle(TextInputStyle.Paragraph);
-			
+				.setCustomId('classicMessage')
+				.setLabel("Rediger un message classic")
+				.setRequired(false)
+				.setStyle(TextInputStyle.Paragraph);
+
 			const firstActionRow = new ActionRowBuilder().addComponents(message);
-			
+
 			// Add inputs to the modal
 			modal.addComponents(firstActionRow);
-			
+
 			// Show the modal to the user
 			await interaction.showModal(modal);
 		} else if (interaction.commandName === "embed_message") {
 			const modalEmbed = new ModalBuilder()
-			.setCustomId('Modal_embedMessage')
-			.setTitle('Nouveau message formatté');
-			
+				.setCustomId('Modal_embedMessage')
+				.setTitle('Nouveau message formatté');
+
 			const embededMessageAuthor = new TextInputBuilder()
-			.setCustomId('embededMessageAuthor')
-			.setLabel("Quel est l'auteur du message formatté")
-			.setRequired(false)
-			.setStyle(TextInputStyle.Short);
-			
+				.setCustomId('embededMessageAuthor')
+				.setLabel("Quel est l'auteur du message formatté")
+				.setRequired(false)
+				.setStyle(TextInputStyle.Short);
+
 			const embededMessageAuthorIcone = new TextInputBuilder()
-			.setCustomId('embededMessageAuthorIcon')
-			.setLabel("Quel est l'icone de l'auteur")
-			.setRequired(false)
-			.setStyle(TextInputStyle.Short);
-			
+				.setCustomId('embededMessageAuthorIcon')
+				.setLabel("Quel est l'icone de l'auteur")
+				.setRequired(false)
+				.setStyle(TextInputStyle.Short);
+
 			const embededMessageTitle = new TextInputBuilder()
-			.setCustomId('embededMessageTitle')
-			.setLabel("Quel est le titre du message formaté")
-			.setRequired(false)
-			.setStyle(TextInputStyle.Short);
-			
+				.setCustomId('embededMessageTitle')
+				.setLabel("Quel est le titre du message formaté")
+				.setRequired(false)
+				.setStyle(TextInputStyle.Short);
+
 			const embededMessage = new TextInputBuilder()
-			.setCustomId('embededMessage')
-			.setLabel("Rediger à la suite un message formaté")
-			.setRequired(true)
-			.setStyle(TextInputStyle.Paragraph);
-			
+				.setCustomId('embededMessage')
+				.setLabel("Rediger à la suite un message formaté")
+				.setRequired(true)
+				.setStyle(TextInputStyle.Paragraph);
+
 			const embededMessageImage = new TextInputBuilder()
-			.setCustomId('image')
-			.setLabel("Image principale")
-			.setRequired(false)
-			.setStyle(TextInputStyle.Short);
-			
+				.setCustomId('image')
+				.setLabel("Image principale")
+				.setRequired(false)
+				.setStyle(TextInputStyle.Short);
+
 			const firstActionRow = new ActionRowBuilder().addComponents(embededMessageAuthor);
 			const secondActionRow = new ActionRowBuilder().addComponents(embededMessageAuthorIcone);
 			const thirdActionRow = new ActionRowBuilder().addComponents(embededMessageTitle);
 			const fourthActionRow = new ActionRowBuilder().addComponents(embededMessage);
 			const firthActionRow = new ActionRowBuilder().addComponents(embededMessageImage);
-			
+
 			modalEmbed.addComponents(firstActionRow, secondActionRow, thirdActionRow, fourthActionRow, firthActionRow);
-			
+
 			await interaction.showModal(modalEmbed);
-			
+
 		} else if (!command) {
 			console.error(`No command matching ${interaction.commandName} was found.`);
 			return;
 		} else {
-			
+
 			try {
 				await command.execute(interaction);
 			} catch (error) {
@@ -120,11 +120,10 @@ client.on(Events.InteractionCreate, async interaction => {
 			try {
 				classicMessage = replaceNitroEmoji(client, classicMessage);
 			}
-			catch(err) {
+			catch (err) {
 				interaction.reply({ content: err.message, ephemeral: true });
 			}
-			finally
-			{
+			finally {
 				interaction.channel.send(classicMessage);
 				interaction.reply({ content: 'Message envoyé', ephemeral: true });
 			}
@@ -136,34 +135,34 @@ client.on(Events.InteractionCreate, async interaction => {
 			let icon = authorIcon;
 			// let Author = 'AOE 4 - Communauté FR';
 			// let icon = 'https://raw.githubusercontent.com/AoE4-Community-Fr/Assets/master/Logo_serveur.png'
-			
+
 			// if (!(author === '' || author === 'AOE 4 - Communauté FR')) {
-				// 	Author = author;
-				// 	icon = authorIcon;
-				// }
-				
+			// 	Author = author;
+			// 	icon = authorIcon;
+			// }
+
 			const title = interaction.fields.getTextInputValue('embededMessageTitle');
 			let message = interaction.fields.getTextInputValue('embededMessage');
 			try {
 				message = replaceNitroEmoji(client, message);
 			}
-			catch(err) {
+			catch (err) {
 				interaction.reply({ content: err.message, ephemeral: true });
 				return;
 			}
 
 			const image = interaction.fields.getTextInputValue('image');
-			
-			
+
+
 			const embed = new EmbedBuilder()
-			.setColor(0x7a7ae0)
-			.setDescription(message)
-			
+				.setColor(0x7a7ae0)
+				.setDescription(message)
+
 			if (Author !== '' && icon !== '') {
 				embed.setAuthor({ name: Author, iconURL: icon })
 			}
 			else if (Author !== '') {
-				embed.setAuthor({ name: Author})
+				embed.setAuthor({ name: Author })
 			}
 			if (image !== '') {
 				embed.setImage(image)
@@ -171,12 +170,12 @@ client.on(Events.InteractionCreate, async interaction => {
 			if (title !== '') {
 				embed.setTitle(title)
 			}
-			
+
 			interaction.channel.send({ embeds: [embed] });
 			interaction.reply({ content: 'Message envoyé', ephemeral: true });
 		}
 	}
-	
+
 });
 
 client.on('threadCreate', post => {
@@ -184,7 +183,7 @@ client.on('threadCreate', post => {
 		const NotifBO_Id = "&1110631444432306288";
 		const AnnounceChannel = client.channels.cache.get(AnnounceChannel_Id);
 		AnnounceChannel.send("||<@" + NotifBO_Id + ">||\nOyé, oyé, bande de troubadours assoiffés de BO. <@" + post.ownerId + "> vient de nous sortir un nouveau breuvage baptisé ***" + post.name +
-		"***, servez-vous dans vos auges et abreuvez-vous de ce savoir : <#" + post.id + ">")
+			"***, servez-vous dans vos auges et abreuvez-vous de ce savoir : <#" + post.id + ">")
 	}
 	else if (post.parentId === ForumCommu_Id && post.type === ChannelType.PublicThread) {
 		const AnnounceChannel = client.channels.cache.get(AnnounceChannel_Id);
@@ -192,6 +191,18 @@ client.on('threadCreate', post => {
 	}
 })
 
+client.on("messageCreate", async message => {
+	if (message.channelId == YT_FR_ID) {
+		const notif_id = "&1109463939055632405";
+
+		message.channel.send("<@" + notif_id + ">");
+
+	}
+	else if (message.channelId = YT_GB_ID) {
+		const notif_id = "&1109464303578402877";
+		message.channel.send("<@" + notif_id + ">");
+	}
+})
 
 client.on("ready", () => {
 	console.log("bot operatrionnel");
